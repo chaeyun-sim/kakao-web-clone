6a16a9e4443031763d9bad34a1c10f0bd1c6e773
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-10-30 00:18:26 +0900

    chore: 기본 환경 세팅

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ['eslint:recommended', 'plugin:storybook/recommended'],
+  plugins: ['react'],
+  rules: {
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
+  },
+};
