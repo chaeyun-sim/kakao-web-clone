bc2c0e9dacf3e3333fe61296caee2b0eb1e260d8
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-11-01 22:11:45 +0900

    style: eslint 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,29 @@
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:storybook/recommended'],
-  plugins: ['react'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:storybook/recommended',
+  ],
+  plugins: ['react', '@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
   rules: {
     'react/jsx-uses-react': 'error',
     'react/jsx-uses-vars': 'error',
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/no-unused-vars': ['error'],
+    'no-unused-vars': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
   },
 };
