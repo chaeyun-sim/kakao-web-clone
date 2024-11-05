57f13f0c4756dff77db03fb9b5e31a9f1135f876
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-11-06 00:04:35 +0900

    style: 인라인 스타일 분리

--- src/components/common/Accordion/ExpandContent.tsx
+++ src/components/common/Accordion/ExpandContent.tsx
@@ -1,16 +1,23 @@
 import { PropsWithChildren } from 'react';
 import { useAccordion } from './context';
+import styled from 'styled-components';
 
 const ExpandContent = ({children}: PropsWithChildren) => {
 	const { isOpen } = useAccordion();
 
 	if (!isOpen) return null;
 	
-	return (
-    <div style={{ width: '100%', display: 'flex', flexDirection: 'column', paddingBottom: '20px', borderBottom: '1px solid #e6e6e6' }}>
-      {children}
-    </div>
-  );
+	return <Content>{children}</Content>;
 };
 
 export default ExpandContent;
+
+const Content = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  padding-bottom: 20px;
+  border-bottom: 1px solid #e6e6e6;
+  font-size: 12px;
+  color: ${({theme}) => theme.color.gray[300]}
+`;
